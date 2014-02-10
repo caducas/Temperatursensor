e7bf5931b3af8ac8cde0984e00ddf022d886eff7
caducas <nikolaus@famkofler.com>
2014-02-10 08:48:03 +0100

    input can now be rounded to decimals (e.g. 2 decimals) modified template file for params design

--- Temperatursensor.js
+++ Temperatursensor.js
@@ -16,26 +16,35 @@ function execute(opts) {
 function listenEvent(eventId, opts) {
 
 	var interval = opts.interval || 5000;
+	var decimals = opts.decimals || -1;
 
-	shellExecutor.exec('cat /sys/bus/w1/devices/w1_bus_master1/w1_master_slaves', function(err, stdout, stderr){
-		if(err !== null) {
-			console.log('TEMPERATURSENSOR: Error initiating device - is the sensor installed correctly?');
-			return;
-		}
+	shellExecutor.exec('sudo modprobe w1-gpio', function(err, stdout, stderr) {
+		shellExecutor.exec('sudo modprobe w1-therm', function(err, stdout, stderr) {
+			shellExecutor.exec('cat /sys/bus/w1/devices/w1_bus_master1/w1_master_slaves', function(err, stdout, stderr){
+				if(err !== null) {
+					return;
+				}
 
-		var device = stdout;
-		device = device.replace(/(\r\n|\n|\r)/gm,"");
 
-		setInterval(function() {
-			var command = 'cat /sys/bus/w1/devices/'+device+'/w1_slave';
-			shellExecutor.exec(command, function(err, out, stderror) {
-				out = out.replace(/(\r\n|\n|\r)/gm,"");
-				var temperature = out.substring(out.length-5);
-				temperature = temperature.substring(0,2) + '.'+temperature.substring(2,5);
-				temperature = parseFloat(temperature);
-				process.emit(eventId+'', eventId, temperature);
+				var device = stdout;
+				device = device.replace(/(\r\n|\n|\r)/gm,"");
+
+				setInterval(function() {
+					var command = 'cat /sys/bus/w1/devices/'+device+'/w1_slave';
+					shellExecutor.exec(command, function(err, out, stderror) {
+						out = out.replace(/(\r\n|\n|\r)/gm,"");
+						var temperature = out.substring(out.length-5);
+						temperature = temperature.substring(0,2) + '.'+temperature.substring(2,5);
+						temperature = parseFloat(temperature);
+
+						if(decimals >=0) {
+							temperature = Number((temperature).toFixed(decimals));
+						}
+						process.emit(eventId+'', eventId, temperature);
+					});
+				},interval);
 			});
-		},interval);
+		});
 	});
 }
 
